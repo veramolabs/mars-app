d12a714dad56118a2b3a2c481bc3b8f79afa0561
Simonas Karuzas <simonas@not.cat>
2020-12-09 14:16:50 +0200

    feat: Copy Credential to another agent

--- src/components/CredentialCard.tsx
+++ src/components/CredentialCard.tsx
@@ -1,17 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { CardHeader, LinearProgress } from "@material-ui/core";
+import { CardActions, CardHeader, IconButton, LinearProgress, Menu, MenuItem } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardActionAreaLink from "./Nav/CardActionAreaLink";
 import Avatar from '@material-ui/core/Avatar';
 import { formatDistanceToNow } from 'date-fns'
 import { UniqueVerifiableCredential } from "daf-typeorm";
 import { IdentityProfile } from "../types";
-import { useAgent } from '../agent'
+import { useAgent, useAgentList } from '../agent'
+import { useHistory } from "react-router-dom";
 
 import PostCredential from "./CredentialCardContent/PostCredential";
 import ProfileCredential from "./CredentialCardContent/ProfileCredential";
 import ReactionCredential from "./CredentialCardContent/ReactionCredential";
 import MessageCredential from "./CredentialCardContent/MessageCredential";
+import CopyIcon from '@material-ui/icons/FileCopy';
 
 interface Props {
   credential: UniqueVerifiableCredential
@@ -20,12 +22,37 @@ interface Props {
 
 function CredentialPostCard(props: Props) {
   const { credential: { verifiableCredential, hash } } = props
+  const history = useHistory()
 
   const { agent } = useAgent()
+  const { agentList, setActiveAgentIndex } = useAgentList()
   const [ loading, setLoading ] = useState(false)
   const [ issuer, setIssuer ] = useState<IdentityProfile>({ did: verifiableCredential.issuer.id })
   const [ subject, setSubject ] = useState<IdentityProfile|undefined>(undefined)
-  
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+
+  const handleClickCopyButton = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuItemClick = async (event: any, index: number) => {
+    // setSelectedIndex(index);
+    try {
+      const result = await agentList[index].agent.dataStoreSaveVerifiableCredential({verifiableCredential})
+      setActiveAgentIndex(index)
+      history.push('/agent/credential/' + result)
+    } catch (e) {
+      console.log(e)
+    }
+    setAnchorEl(null);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   useEffect(() => {
     setLoading(true)
     Promise.all<IdentityProfile, IdentityProfile>([
@@ -53,10 +80,12 @@ function CredentialPostCard(props: Props) {
   } else if (verifiableCredential.type.includes('Message')) {
     contents = (<MessageCredential  {...props} issuer={issuer} subject={subject} /> )
   } 
+
+  
   
   return (
     <Card elevation={2}>
-      <CardActionAreaLink to={'/identity/' + issuer.did}>
+      <CardActionAreaLink to={'/agent/identity/' + issuer.did}>
         <CardHeader
           avatar={
             <Avatar src={issuer.picture} />
@@ -65,9 +94,31 @@ function CredentialPostCard(props: Props) {
           subheader={`${issuer.nickname} | ${formatDistanceToNow(Date.parse(verifiableCredential.issuanceDate))} ago`}
         />
       </CardActionAreaLink>
-      <CardActionAreaLink to={ props.type === 'summary' ? '/credential/' + hash : '/identity/' + subject?.did}>
+      <CardActionAreaLink to={ props.type === 'summary' ? '/agent/credential/' + hash : '/agent/identity/' + subject?.did}>
         {contents}
       </CardActionAreaLink>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Copy" onClick={handleClickCopyButton}>
+          <CopyIcon />
+        </IconButton>
+      </CardActions>
+      <Menu
+        id="lock-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {agentList.map((option, index) => (
+          <MenuItem
+            key={index}
+            disabled={!option.agent.availableMethods().includes('dataStoreSaveVerifiableCredential')}
+            onClick={(event) => handleMenuItemClick(event, index)}
+          >
+            {option.name}
+          </MenuItem>
+        ))}
+      </Menu>
     </Card>
   )
 }

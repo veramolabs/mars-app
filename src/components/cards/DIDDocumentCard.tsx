2dec016e2b1aa7cf7e45dd49394c5b55ee516f09
Simonas Karuzas <simonas@not.cat>
2021-01-06 20:33:34 +0200

    feat: add service

--- src/components/cards/DIDDocumentCard.tsx
+++ src/components/cards/DIDDocumentCard.tsx
@@ -1,8 +1,8 @@
-import { Box, List, ListItem, ListItemAvatar, ListItemText, ListSubheader, TextField, CardActions, makeStyles, ListItemSecondaryAction, IconButton, ListItemIcon, Menu, MenuItem, Typography } from '@material-ui/core'
+import { Box, List, ListItem, ListItemAvatar, ListItemText, ListSubheader, TextField, CardActions, makeStyles, ListItemSecondaryAction, IconButton, ListItemIcon, Menu, MenuItem, Typography, Button, Dialog, DialogActions, DialogContent, useMediaQuery, useTheme, DialogTitle } from '@material-ui/core'
 import { ToggleButtonGroup, ToggleButton } from '@material-ui/lab'
 import { DIDDocument } from 'did-resolver'
-import React from 'react'
-
+import React, { useState } from 'react'
+import shortId from 'shortid'
 import MessageIcon from '@material-ui/icons/Message'
 import SettingsIcon from '@material-ui/icons/Settings'
 import CheckCircleIcon from '@material-ui/icons/CheckCircle';
@@ -10,6 +10,8 @@ import VpnKeyIcon from '@material-ui/icons/VpnKey'
 import CodeIcon from '@material-ui/icons/Code'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreIcon from '@material-ui/icons/MoreVert'
+import { useSnackbar } from 'notistack'
+import { useAgent } from '../../agent'
 
 const useStyles = makeStyles((theme) => ({
   cardActions: {
@@ -23,6 +25,16 @@ const useStyles = makeStyles((theme) => ({
 function DIDDocumentCard({ didDoc, isManaged }: { didDoc: DIDDocument, isManaged?: boolean }) {
   const classes = useStyles()
   const [anchorEl, setAnchorEl] = React.useState(null)
+  const { enqueueSnackbar } = useSnackbar()
+  const theme = useTheme()
+  const { agent } = useAgent()
+  const fullScreen = useMediaQuery(theme.breakpoints.down('xs'))
+  const [showServiceModal, setShowServiceModal] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [serviceId, setServiceId] = useState(didDoc.id + '#' + shortId.generate())
+  const [serviceType, setServiceType] = useState('')
+  const [serviceEndpoint, setServiceEndpoint] = useState('')
+  const [serviceDescription, setServiceDescription] = useState('')
 
   const [cardType, setCardType] = React.useState<'preview' | 'source'>('preview')
   const handleClose = () => {
@@ -31,6 +43,26 @@ function DIDDocumentCard({ didDoc, isManaged }: { didDoc: DIDDocument, isManaged
 
   const handleOpen = (event: any) => {
     setAnchorEl(event.currentTarget)
+  }
+
+  const addService = async () => {
+    setLoading(true)
+    try {
+      await agent.didManagerAddService({
+        did: didDoc.id,
+        service: {
+          id: serviceId,
+          type: serviceType,
+          serviceEndpoint,
+          description: serviceDescription
+        }
+      })
+      setShowServiceModal(false)
+      enqueueSnackbar('Service added', { variant: 'success' })
+    } catch (error) {
+      enqueueSnackbar(error.message, { variant: 'error' })
+    }
+    setLoading(false)
   }
 
   return (
@@ -152,7 +184,7 @@ function DIDDocumentCard({ didDoc, isManaged }: { didDoc: DIDDocument, isManaged
       </CardActions>
 
       <Menu id="lock-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        <MenuItem onClick={() => { }}>
+        <MenuItem onClick={() => { setShowServiceModal(true) }}>
           <ListItemIcon>
             <SettingsIcon />
           </ListItemIcon>
@@ -171,6 +203,90 @@ function DIDDocumentCard({ didDoc, isManaged }: { didDoc: DIDDocument, isManaged
 
 
       </Menu>
+
+      <Dialog
+        fullScreen={fullScreen}
+        open={showServiceModal}
+        onClose={() => {
+          setShowServiceModal(false)
+        }}
+        maxWidth="sm"
+        fullWidth
+        scroll='paper'
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle>Add service</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="DID"
+            value={didDoc.id}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            disabled
+            aria-readonly
+          />
+          <TextField
+            label="Service ID"
+            value={serviceId}
+            onChange={(e) => setServiceId(e.target.value)}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+
+          <TextField
+            label="Service type"
+            placeholder='Messaging'
+            value={serviceType}
+            onChange={(e) => setServiceType(e.target.value)}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+
+          <TextField
+            label="Service endpoint"
+            placeholder='https://example.com/messaging'
+            value={serviceEndpoint}
+            onChange={(e) => setServiceEndpoint(e.target.value)}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+
+          <TextField
+            label="Service description"
+            placeholder='Handles incoming POST messages'
+            value={serviceDescription}
+            onChange={(e) => setServiceDescription(e.target.value)}
+            fullWidth
+            margin="normal"
+            variant="outlined"
+          />
+
+        </DialogContent>
+        <DialogActions>
+          <Button
+            autoFocus
+            onClick={() => {
+              setShowServiceModal(false)
+            }}
+            color="default"
+
+          >
+            Close
+          </Button>
+          <Button
+            autoFocus
+            onClick={addService}
+            color="primary"
+
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
 
     </Box>
 

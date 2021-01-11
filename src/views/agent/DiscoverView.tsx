09416e0f5cd2517fe0a05476bdb7783bb171523f
Simonas Karuzas <simonas@not.cat>
2021-01-11 10:27:49 +0200

    fix: default resolve did

--- src/views/agent/DiscoverView.tsx
+++ src/views/agent/DiscoverView.tsx
@@ -42,7 +42,7 @@ function DiscoverView() {
   const { enqueueSnackbar } = useSnackbar()
   const { agent } = useAgent()
   const [loading, setLoading] = useState(false)
-  const [didUrl, setDidUrl] = useState<string>('did:web:simonas.not.cat')
+  const [didUrl, setDidUrl] = useState<string>('did:web:sun.veramo.io')
   const [didDoc, setDidDoc] = useState<DIDDocument | undefined>(undefined)
   const classes = useStyles()
 
